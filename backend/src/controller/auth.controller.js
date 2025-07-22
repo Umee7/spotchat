1d09e9f489a58251d9f1a6a0525878f8eeed5582
Umee <umeshmessi23@gmail.com>
2025-07-22 20:55:59 +0530

    Added authCallback controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,3 +1,24 @@
-export const getAuth = (req, res) => {
-    res.send("Auth route with GET method");
-}
+import { User } from "../models/user.model.js"
+
+export const authCallback = async (req, res) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
+
+    // check if user already exists
+    const user = await User.findOne({ clerkId: id });
+
+    // User does not exist, create a new user
+    if (!user) {
+      await User.create({
+        clerkId: id,
+        fullName: `${firstName} ${lastName}`,
+        imageUrl,
+      });
+    }
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log("error in auth callback", error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
        const conn =  await mongoose.connect(process.env.MONGODB_URI);
-       console.log("Connected to mongodb")
+       console.log(`Connected to mongoDB ${conn.connection.host}`)
     } catch(error) {
-        process.exit(1);
         console.log("Failed to connect mongoDB")
+        process.exit(1); // 1 is for failure , 0 is for success
     }
 }
