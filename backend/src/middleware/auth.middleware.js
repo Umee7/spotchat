3e3f5fd87a8d46cd97551a38b5acbc57841b0aab
Umee <umeshmessi23@gmail.com>
2026-03-14 20:55:24 +0530

    Chore: Update .env file and add catch for auth.middleware.js

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,23 +1,28 @@
-import { clerkClient} from "@clerk/express"
+import { clerkClient } from "@clerk/express";
 
+export const protectRoute = async (req, res, next) => {
+  if (!req.auth.userId) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized - you must be logged in" });
+  }
+  next();
+};
 
-export const protectRoute = (req, res, next) => {
-    if(!req.auth.userId) {
-        return res.status(401).json({message: "Unauthorized - you must be logged in"});
+export const requireAdmin = async (req, res, next) => {
+  try {
+    const currentUser = await clerkClient.users.getUser(req.auth.userId);
+    const isAdmin =
+      process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress?.emailAddress;
+
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized - you must be an admin" });
     }
 
     next();
-}
-
-export const requireAdmin = async (req, res, next) => {
-    try {
-        const currentUser = await clerkClient.users.getUsers(req.auth.userId);
-        const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress?.emailAddress;
-
-        if(!isAdmin) {
-            return res.status(403).json({message: "Unauthorized - you must be an admin"});
-        }
-    } catch (error) {
-        
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
