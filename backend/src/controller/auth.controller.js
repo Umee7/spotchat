eca509f393c5cbb54f9ae88f25d693dc1f280ad3
Umee <umeshmessi23@gmail.com>
2026-03-15 17:44:37 +0530

    fix: correct admin routes and update auth controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,24 +1,24 @@
-import { User } from "../models/user.model.js"
+import { User } from "../models/user.model.js";
 
-export const authCallback = async (req, res) => {
+export const authCallback = async (req, res, next) => {
   try {
     const { id, firstName, lastName, imageUrl } = req.body;
 
     // check if user already exists
     const user = await User.findOne({ clerkId: id });
 
-    // User does not exist, create a new user
     if (!user) {
+      // signup
       await User.create({
         clerkId: id,
-        fullName: `${firstName} ${lastName}`,
+        fullName: `${firstName || ""} ${lastName || ""}`.trim(),
         imageUrl,
       });
     }
 
     res.status(200).json({ success: true });
   } catch (error) {
-    console.log("error in auth callback", error);
-    res.status(500).json({ message: "Internal server error", error });
+    console.log("Error in auth callback", error);
+    next(error);
   }
 };
